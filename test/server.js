30c0fe53b31fa70ab63f19e1515d49959ecf3c79
tenterrain <153330685@qq.com>
2016-03-24 11:46:47 +0800

    fix a bug

--- test/server.js
+++ test/server.js
@@ -1,30 +1,68 @@
 "use strict"
 
-const http = require('http');
-const util = require('util');
-const url = require('url');
-const fs = require('fs');
-const server = http.createServer();
+const net = require('net');
+const utils = require('../lib/utils');
 const PORT = process.env.PORT || 80;
 
-server.on('request', (req, res) => {
-    res.writeHead(200, 'Ok');
-    let stream = fs.createReadStream('static/index.html')
-    stream.pipe(res);
+const server = net.createServer({
+    allowHalfOpen: true
+}, connect => {
+    connect.write('HTTP/1.1 200 0k\r\n');
+    connect.write('Transfer-Encoding: chunked\r\n');
+    connect.write('\r\n');
+    connect.write('6\r\n');
+    connect.write('hello \r\n');
+    connect.write('6\r\n');
+    setTimeout(() => {
+        connect.write('world!\r\n');
+        connect.write('0\r\n');
+        connect.end();
+    }, 1000);
 
-    req.reqChunks = [];
+    let chunks = [];
     
-    req.on('data', (data) => {
-        req.reqChunks.push(data);
+    connect.on('data', (data) => {
+        chunks.push(data);
     });
 
-    req.on('end', () => {
-        console.log(`request url: ${req.url}`);
-        console.log(req.headers);
-
-        let reqStr = Buffer.concat(req.reqChunks).toString('utf8');
-        console.log(reqStr);
+    connect.on('end', () => {
+        console.log(utils.parseResTxt(Buffer.concat(chunks).toString('utf8')));
     });
 });
 
 server.listen(PORT, () => console.log(`server start on port: ${PORT}`));
+
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+// const PORT = process.env.PORT || 80;
+
+// const server = http.createServer((req, res) => {
+//     // res.writeHead(200, 'Ok');
+//     // let stream = fs.createReadStream('static/index.html');
+//     // stream.pipe(res);
+
+//     res.writeHead(200, 'Ok', {'Transfer-Encoding': 'chunked'});
+//     res.write('\r\n');
+//     res.write('6\r\n');
+//     res.write('hello \r\n');
+//     res.write('6\r\n');
+//     setTimeout(() => {
+//         res.write('world!\r\n');
+//         res.write('0\r\n');
+//         res.end();
+//     }, 500);
+
+//     let chunks = [];
+//     req.on('data', data => {
+//         chunks.push(data);
+//     });
+
+//     req.on('end', () => {
+//         console.log(req.url);
+//         console.log(req.headers);
+//         console.log(Buffer.concat(chunks).toString('utf8'));
+//     });
+// });
+
+// server.listen(PORT, () => console.log(`server start on port: ${PORT}`));
